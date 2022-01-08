9953f00c13188c679b92f180d44fd45c4225d1be
Kiril <kola201510@gmail.com>
2022-01-08 19:04:10 +0200

    :|

--- src/app.js
+++ src/app.js
@@ -3,7 +3,8 @@ import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
 import {VRButton} from "three/examples/jsm/webxr/VRButton"
 import {BoxLineGeometry} from "three/examples/jsm/geometries/BoxLineGeometry"
 import {XRControllerModelFactory} from "three/examples/jsm/webxr/XRControllerModelFactory";
-import flashLightPack from "../assets/KatanaForZIP.glb"
+import forkPack from "../assets/Fork.glb"
+import flashLightPack from "../assets/flash-light.glb"
 import officeChairGlb from "/assets/Cute Cartoon Character.glb"
 import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader";
 import {controllers} from "three/examples/jsm/libs/dat.gui.module";
@@ -103,7 +104,7 @@ class App {
 
     const geometry = new THREE.IcosahedronBufferGeometry(this.radius, 2)
 
-    for (let i = 0; i < 80; i++) {
+    for (let i = 0; i < 8; i++) {
 
       const objects = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial({ color: Math.random() * 0xffffff } ))
 
@@ -153,10 +154,31 @@ class App {
     document.body.appendChild( VRButton.createButton(this.renderer) )
 
     let i = 0
-    //this.flashLightController(i++)
+    this.forkController(i++)
     this.buildStandardController(i++)
-    this.flashLightController(i++)
+    // this.flashLightController(i++)
     //this.buildStandardController(i++)
+  }
+
+  forkController(index) {
+    const self = this
+    let controller = this.renderer.xr.getController(index)
+
+    controller.addEventListener( 'connected', function (event) {
+      self.buildForkController.call(self, event.data, this)
+    })
+
+    controller.addEventListener( 'disconnected', function () {
+      while(this.children.length > 0) {
+        this.remove(this.children[0])
+        const controllerIndex = self.controllers.indexOf(this)
+        self.controllers[controllerIndex] = null
+      }
+    })
+    controller.handle = () => {}
+
+    this.controllers[index] = controller
+    this.scene.add(controller)
   }
 
   flashLightController(index) {
@@ -200,6 +222,44 @@ class App {
     this.scene.add(controller)
   }
 
+  buildForkController(data, controller) {
+    let geometry, material, loader
+
+    const self = this
+
+    if (data.targetRayMode === 'tracked-pointer') {
+      loader = new GLTFLoader()
+      loader.load(forkPack, (gltf) => {
+        const fork = gltf.scene
+        const scale = 0.05
+        fork.scale.set(scale, scale, scale)
+        fork.rotation.set(0, Math.PI / -180 * 90 , 0)
+        controller.add(fork)
+        const spotlightGroup = new THREE.Group()
+        self.spotlights[controller.uuid] = spotlightGroup
+
+        const spotlight = new THREE.SpotLight(0xFFFFFF, 2, 12, Math.PI / 15, 0.3)
+        spotlight.position.set(0, 0, 0)
+        spotlight.target.position.set(0, 0, -1)
+        spotlightGroup.add(spotlight.target)
+        spotlightGroup.add(spotlight)
+        controller.add(spotlightGroup)
+
+        spotlightGroup.visible = false
+
+        geometry = new THREE.CylinderBufferGeometry(0.03, 1, 5, 32, true)
+        geometry.rotateX(Math.PI / 2)
+        material = new SpotLightVolumetricMaterial()
+        const cone = new THREE.Mesh(geometry, material)
+        cone.translateZ(-2.6)
+        spotlightGroup.add(cone)
+
+    }, null,
+          (error) => console.error(`An error happened: ${error}`)
+      )
+   }
+  }
+
   buildFlashLightController(data, controller) {
     let geometry, material, loader
 
@@ -209,7 +269,7 @@ class App {
       loader = new GLTFLoader()
           loader.load(flashLightPack, (gltf) => {
             const flashLight = gltf.scene.children[2]
-            const scale = 0.8
+            const scale = 0.6
             flashLight.scale.set(scale, scale, scale)
             controller.add(flashLight)
             const spotlightGroup = new THREE.Group()
